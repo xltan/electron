7fdb8d295bd138b400546e89c7b1bdf7cb935973
Shelley Vohr <shelley.vohr@gmail.com>
2022-04-11 13:06:40 +0200

    chore: fix pageRanges param

--- spec-main/api-web-contents-spec.ts
+++ spec-main/api-web-contents-spec.ts
@@ -1871,10 +1871,7 @@ describe('webContents module', () => {
 
       it('respects custom settings', async () => {
         const data = await w.webContents.printToPDF({
-          pageRanges: {
-            from: 0,
-            to: 2
-          },
+          pageRanges: '1-3',
           landscape: true
         });
 
